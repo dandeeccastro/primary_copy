297878aa5652ddea9a3b515a0ae3503c713936c1
Danilo Collares <dundee@localhost.localdomain>
2020-11-05 16:01:59 -0300

    (feature) Comandos locais e soquete ouvinte

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import select
 import re
 
+def formatHistoryData(historyData):
+    print(historyData)
+
 def validateCommand(fullCommand):
     command = fullCommand.split()
     if command[0] == 'read':
@@ -19,9 +22,15 @@ def validateCommand(fullCommand):
     return bool(match)
 
 def main(nodeID):
-    print("[Node {0}] TODO implement me".format(nodeID))
+    X = 0
+    history = []
+    hasWritePermission = True
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.bind( ("localhost", 4200 + int(nodeID)) )
+    listener.listen(1)
+
     while True:
-        inputs = [sys.stdin]
+        inputs = [sys.stdin, listener]
         r,w,x = select.select(inputs,[],[])
 
         for command in r:
@@ -31,14 +40,32 @@ def main(nodeID):
                 commandIsValid = validateCommand(fullMessage)
 
                 if message[0] == 'read' and commandIsValid:
-                    print("Ok {0}".format(message[0]))
+                    print("[Node {0}] X = {1}".format(nodeID,X))
                 elif message[0] == 'history' and commandIsValid:
-                    print("Ok {0}".format(message[0]))
+                    response = "[Node {0}]\n".format(nodeID)
+                    if history:
+                        for entry in history:
+                            response += "Node {0} escreveu {1} \n".format(entry[0],entry[1])
+                    else:
+                        response += " X não foi alterado ainda"
+                    print(response)
                 elif message[0] == 'write' and commandIsValid:
-                    print("Ok {0}".format(message[0]))
+                    if hasWritePermission:
+                        X = int(message[1])
+                        history.append((nodeID,int(message[1])))
+                    print("[Node {0}] Escreveu {1} em X".format(nodeID,int(message[1])))
                 elif message[0] == 'close' and commandIsValid:
                     print("Ok {0}".format(message[0]))
                     sys.exit()
+
+            elif command == listener:
+                new_sock, addr = listener.accept()
+                fullMsg = new_sock.recv(1024).decode('utf-8')
+                msg = fullMsg.split()
+
+                if msg[0] == 'WRITE':
+                    X = int(msg[1])
+                    historyUpdate = formatHistoryData(msg[2])
 
 if __name__ == '__main__':
     nodeID = input('Insira o ID desse nó: ')
